ccb89e07b143e22bbb8074f67ae7b98177132f2f
RomanKosov <kosovr48@gmail.com>
2023-11-27 20:18:33 +0500

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 13, 452, 345, 68, 19, 345, 0, 41, 385, 70, 345, 4 };
+int n = array.Length;
+int find = 345;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
